558c532ca3a1b01a1f9942b65c32f9b4563915ef
perebati <lucas.bpereira1999@gmail.com>
2025-02-28 20:43:26 -0300

    [Feat]: Create input domain model and predefined types

---
+++ src/main/java/com/git/rule_forge/modules/input/domain/predefined/PredefinedInputExampleEntity.java
@@ -0,0 +1,25 @@
+package com.git.rule_forge.modules.input.domain.predefined;
+
+import lombok.Data;
+
+/**
+ * An example of what a predefined input can be.
+ *
+ * @author Lucas Batista Pereira
+ * @version v1.0
+ * @class PredefinedInputExampleEntity
+ * @since v1.0 (30/11/2024)
+ */
+@Data
+public class PredefinedInputExampleEntity {
+    private String CPF;
+    private Integer idade;
+    private Endereco endereco;
+
+    @Data
+    static class Endereco {
+        private String rua;
+        private String bairro;
+    }
+}
+
