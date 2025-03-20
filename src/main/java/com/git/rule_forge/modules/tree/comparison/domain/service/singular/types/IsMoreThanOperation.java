344cf5ad41ab39a6d0fe5a3110d5b08c21d27444
perebati <lucas.bpereira1999@gmail.com>
2025-03-20 08:03:33 -0300

    [Feat]: Implement greater than comparison operations

---
+++ src/main/java/com/git/rule_forge/modules/tree/comparison/domain/service/singular/types/IsMoreThanOperation.java
@@ -0,0 +1,23 @@
+package com.git.rule_forge.modules.tree.comparison.domain.service.singular.types;
+
+import com.git.rule_forge.modules.tree.comparison.domain.service.singular.ComparisonSingularOperation;
+
+/**
+ * @author Lucas Batista Pereira
+ * @version v1.0
+ * @class IsMoreThanOperation
+ * @since v1.0 (30/11/2024)
+ */
+public class IsMoreThanOperation extends ComparisonSingularOperation {
+    @Override
+    public Boolean doOperation(String a, String b) {
+        Number valueA = parseNumericValue(a);
+        Number valueB = parseNumericValue(b);
+
+        if (valueA == null || valueB == null) {
+            throw new IllegalArgumentException("Não foi possível converter um ou ambos os valores para tipos numéricos.");
+        }
+
+        return Double.compare(valueA.doubleValue(), valueB.doubleValue()) > 0;
+    }
+}
