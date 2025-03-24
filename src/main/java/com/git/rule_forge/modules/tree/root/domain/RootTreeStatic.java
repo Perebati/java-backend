42987e42859e1e3b2c2dcc2f324d4031f13b6268
perebati <lucas.bpereira1999@gmail.com>
2025-03-24 03:12:51 -0300

    [Feat]: Create static and dynamic root tree implementations

---
+++ src/main/java/com/git/rule_forge/modules/tree/root/domain/RootTreeStatic.java
@@ -0,0 +1,62 @@
+package com.git.rule_forge.modules.tree.root.domain;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import com.git.rule_forge.modules.input.domain.predefined.PredefinedTypeEnum;
+import com.git.rule_forge.modules.tree._shared.domain.NodeInput;
+import com.git.rule_forge.modules.tree._shared.domain.NodeTree;
+import com.git.rule_forge.modules.tree._shared.domain.RootTree;
+import com.git.rule_forge.utils.annotations.Default;
+
+import java.util.List;
+
+/**
+ * Maps Root attributes if the input of the validation tree is predefined.
+ *
+ * @author Lucas Batista Pereira
+ * @version v1.0
+ * @class RootTreeStatic
+ * @since v1.0 (30/11/2024)
+ */
+@Getter
+@Setter
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+@JsonTypeName("RootTreeStatic")
+@Schema(name = "RootTreeStatic", description = "Representa um nó raiz onde o input é pré-definido na árvore de nós")
+public class RootTreeStatic extends RootTree<NodeInput> {
+    private PredefinedTypeEnum input_type;
+
+    @Default
+    public RootTreeStatic(
+            String name,
+            PredefinedTypeEnum type
+    ) {
+        super(name, NodeType.ROOT_STATIC);
+        this.input_type = type;
+    }
+
+    @JsonCreator
+    public RootTreeStatic(
+            @JsonProperty("name") String name,
+            @JsonProperty("input_type") PredefinedTypeEnum type,
+            @JsonProperty("nodes") List<NodeTree<NodeInput>> nodes
+    ) {
+        super(name, NodeType.ROOT_STATIC, nodes);
+        this.input_type = type;
+    }
+
+    @Override
+    public boolean traverse(NodeInput inputData) {
+        for (NodeTree<NodeInput> node : getNodes()) {
+            if (!node.traverse(inputData)) return false;
+        }
+        return true;
+    }
+}
