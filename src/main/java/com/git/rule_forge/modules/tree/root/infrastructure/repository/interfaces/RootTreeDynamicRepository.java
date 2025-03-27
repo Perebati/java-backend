fae03a260b885e03b4f6f8c46b1e69043b428c63
perebati <lucas.bpereira1999@gmail.com>
2025-03-27 18:58:34 -0300

    [Feat]: Define root tree repository interfaces

---
+++ src/main/java/com/git/rule_forge/modules/tree/root/infrastructure/repository/interfaces/RootTreeDynamicRepository.java
@@ -0,0 +1,14 @@
+package com.git.rule_forge.modules.tree.root.infrastructure.repository.interfaces;
+
+
+import com.git.rule_forge._shared.infrastructure.repository.interfaces.GenericBusinessRepository;
+import com.git.rule_forge.modules.tree.root.domain.RootTreeDynamic;
+
+/**
+ * @author Lucas Batista Pereira
+ * @version v1.0
+ * @class RootTreeDynamicRepository
+ * @since v1.0 (30/11/2024)
+ */
+public interface RootTreeDynamicRepository extends GenericBusinessRepository<RootTreeDynamic> {
+}
---
+++ src/main/java/com/git/rule_forge/modules/tree/root/infrastructure/repository/interfaces/RootTreeStaticRepository.java
@@ -0,0 +1,13 @@
+package com.git.rule_forge.modules.tree.root.infrastructure.repository.interfaces;
+
+import com.git.rule_forge._shared.infrastructure.repository.interfaces.GenericBusinessRepository;
+import com.git.rule_forge.modules.tree.root.domain.RootTreeStatic;
+
+/**
+ * @author Lucas Batista Pereira
+ * @version v1.0
+ * @class RootTreeStaticRepository
+ * @since v1.0 (30/11/2024)
+ */
+public interface RootTreeStaticRepository extends GenericBusinessRepository<RootTreeStatic> {
+}
