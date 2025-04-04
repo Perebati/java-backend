ea88cb38baa32cfe7fbf7f4cb2023be308c12e22
perebati <lucas.bpereira1999@gmail.com>
2025-04-04 07:43:26 -0300

    [Feat]: Create root tree REST controller

---
+++ src/main/java/com/git/rule_forge/modules/tree/root/adapter/web/controller/RootTreeControllerImpl.java
@@ -0,0 +1,69 @@
+package com.git.rule_forge.modules.tree.root.adapter.web.controller;
+
+import com.git.rule_forge.exception.SystemGlobalException;
+import com.git.rule_forge.exception.models.AdapterException;
+import com.git.rule_forge.modules.tree.root.adapter.web.controller.interfaces.RootTreeController;
+import com.git.rule_forge.modules.tree.root.adapter.web.request.RootCreationDynamicInputDTO;
+import com.git.rule_forge.modules.tree.root.adapter.web.request.RootCreationPredefinedInputDTO;
+import com.git.rule_forge.modules.tree.root.application.service.interfaces.RootTreeDynamicService;
+import com.git.rule_forge.modules.tree.root.application.service.interfaces.RootTreeStaticService;
+import com.git.rule_forge.modules.tree.root.domain.RootTreeDynamic;
+import com.git.rule_forge.modules.tree.root.domain.RootTreeStatic;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+
+/**
+ * @author Lucas Batista Pereira
+ * @version v1.0
+ * @class RootTreeControllerImpl
+ * @since v1.0 (30/11/2024)
+ */
+@Controller
+class RootTreeControllerImpl implements RootTreeController {
+    private final RootTreeStaticService rootTreeStaticService;
+    private final RootTreeDynamicService rootTreeDynamicService;
+
+    RootTreeControllerImpl(
+            RootTreeStaticService rootTreeStaticService,
+            RootTreeDynamicService rootTreeDynamicService) {
+        this.rootTreeStaticService = rootTreeStaticService;
+        this.rootTreeDynamicService = rootTreeDynamicService;
+    }
+
+    @Override
+    public ResponseEntity<RootTreeStatic> createRootPredefined(
+            @NonNull @RequestBody RootCreationPredefinedInputDTO request) throws SystemGlobalException {
+        try {
+            return ResponseEntity.status(HttpStatus.CREATED).body(
+                    this.rootTreeStaticService.create(
+                            request.name(),
+                            request.type(),
+                             true));
+        } catch (SystemGlobalException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new AdapterException("Unexpected error in static root creation.", e);
+        }
+    }
+
+    @Override
+    public ResponseEntity<RootTreeDynamic> createRootDynamic(
+            @NonNull @RequestBody RootCreationDynamicInputDTO request) throws SystemGlobalException {
+        try {
+            return ResponseEntity.status(HttpStatus.CREATED).body(
+                    this.rootTreeDynamicService.create(
+                            request.name(),
+                            request.dynamicInput_id(),
+                            true
+                    ));
+        } catch (SystemGlobalException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new AdapterException("Unexpected error in dynamic root creation.", e);
+        }
+    }
+}
+
