0eb9ee14fb8c1799aabed6fb7c6e3754f55e8356
perebati <lucas.bpereira1999@gmail.com>
2025-04-03 21:07:56 -0300

    [Feat]: Add root tree web request DTOs

---
+++ src/main/java/com/git/rule_forge/modules/tree/root/adapter/web/request/RootCreationDynamicInputDTO.java
@@ -0,0 +1,16 @@
+package com.git.rule_forge.modules.tree.root.adapter.web.request;
+
+import org.springframework.lang.NonNull;
+
+import java.util.UUID;
+
+/**
+ * @author Lucas Batista Pereira
+ * @version v1.0
+ * @record RootCreationDynamicInputDTO
+ * @since v1.0 (04/11/2024)
+ */
+public record RootCreationDynamicInputDTO(
+        @NonNull String name,
+        @NonNull UUID dynamicInput_id) {
+}
---
+++ src/main/java/com/git/rule_forge/modules/tree/root/adapter/web/request/RootCreationPredefinedInputDTO.java
@@ -0,0 +1,15 @@
+package com.git.rule_forge.modules.tree.root.adapter.web.request;
+
+import com.git.rule_forge.modules.input.domain.predefined.PredefinedTypeEnum;
+import org.springframework.lang.NonNull;
+
+/**
+ * @author Lucas Batista Pereira
+ * @version v1.0
+ * @class RootCreationDTO
+ * @since 01/11/2024
+ */
+public record RootCreationPredefinedInputDTO(
+        @NonNull String name,
+        @NonNull PredefinedTypeEnum type) {
+}
