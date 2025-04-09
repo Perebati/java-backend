77ee6c973c25879abbc95cceb7befe99356ed971
perebati <lucas.bpereira1999@gmail.com>
2025-04-09 15:49:13 -0300

    [Feat]: Add node tree mappers

---
+++ src/main/java/com/git/rule_forge/modules/tree/node/infrastructure/mapper/NodeTreeIfMapper.java
@@ -0,0 +1,29 @@
+package com.git.rule_forge.modules.tree.node.infrastructure.mapper;
+
+import com.git.rule_forge.modules.tree._shared.infrastructure.mapper.NodeTreeGenericMapper;
+import com.git.rule_forge.modules.tree.node.domain.NodeTreeIf;
+import com.git.rule_forge.modules.tree.node.infrastructure.NodeTreeIfEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.ReportingPolicy;
+import org.springframework.context.annotation.Primary;
+
+/**
+ * @author Lucas Batista Pereira
+ * @version v1.1
+ * @class NodeTreeIfMapper
+ * @since v1.0 (06/02/2025)
+ */
+@Mapper(
+        componentModel = MappingConstants.ComponentModel.SPRING,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
+@Primary
+public interface NodeTreeIfMapper extends NodeTreeGenericMapper<NodeTreeIf, NodeTreeIfEntity> {
+    @Override
+    @Mapping(target = "conditionalNodes", source = "conditionalNodes", qualifiedByName = "mapNodes")
+    @Mapping(target = "thenNodes", source = "thenNodes", qualifiedByName = "mapNodes")
+    @Mapping(target = "elseNodes", source = "elseNodes", qualifiedByName = "mapNodes")
+    NodeTreeIf toEntity(NodeTreeIfEntity schema);
+}
