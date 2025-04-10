9816ce8713adf7c26c9c5135312cb163bdd1a131
perebati <lucas.bpereira1999@gmail.com>
2025-04-10 11:32:36 -0300

    [Feat]: Define node tree repository interfaces

---
+++ src/main/java/com/git/rule_forge/modules/tree/node/infrastructure/repository/interfaces/NodeTreeBlockRepository.java
@@ -0,0 +1,13 @@
+package com.git.rule_forge.modules.tree.node.infrastructure.repository.interfaces;
+
+import com.git.rule_forge._shared.infrastructure.repository.interfaces.GenericBusinessRepository;
+import com.git.rule_forge.modules.tree.node.domain.NodeTreeBlock;
+
+/**
+ * @author Lucas Batista Pereira
+ * @version v1.0
+ * @class NodeTreeBlockRepository
+ * @since v1.0 (30/11/2024)
+ */
+public interface NodeTreeBlockRepository extends GenericBusinessRepository<NodeTreeBlock> {
+}
---
+++ src/main/java/com/git/rule_forge/modules/tree/node/infrastructure/repository/interfaces/NodeTreeIfRepository.java
@@ -0,0 +1,13 @@
+package com.git.rule_forge.modules.tree.node.infrastructure.repository.interfaces;
+
+import com.git.rule_forge._shared.infrastructure.repository.interfaces.GenericBusinessRepository;
+import com.git.rule_forge.modules.tree.node.domain.NodeTreeIf;
+
+/**
+ * @author Lucas Batista Pereira
+ * @version v1.1
+ * @class NodeTreeIfRepository
+ * @since v1.0 (06/02/2025)
+ */
+public interface NodeTreeIfRepository extends GenericBusinessRepository<NodeTreeIf> {
+}
