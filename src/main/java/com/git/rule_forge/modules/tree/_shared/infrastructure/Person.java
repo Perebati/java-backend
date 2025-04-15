ce91d39b4f9f95d1eb4b2dec2a4730621f3e7cef
perebati <lucas.bpereira1999@gmail.com>
2025-04-15 09:06:28 -0300

    [Feat]: Add tree shared infrastructure entities

---
+++ src/main/java/com/git/rule_forge/modules/tree/_shared/infrastructure/Person.java
@@ -0,0 +1,8 @@
+package com.git.rule_forge.modules.tree._shared.infrastructure;
+
+import jakarta.persistence.Entity;
+import com.git.rule_forge._shared.infrastructure.GenericBusinessEntity;
+
+@Entity
+public class Person extends GenericBusinessEntity {
+}
