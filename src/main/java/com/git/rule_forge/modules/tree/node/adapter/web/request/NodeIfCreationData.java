b8082568aff2348d0278f40fb2e33eddcb3a0463
perebati <lucas.bpereira1999@gmail.com>
2025-04-17 03:07:18 -0300

    [Feat]: Add node tree web request DTOs

---
+++ src/main/java/com/git/rule_forge/modules/tree/node/adapter/web/request/NodeIfCreationData.java
@@ -0,0 +1,15 @@
+package com.git.rule_forge.modules.tree.node.adapter.web.request;
+
+import com.git.rule_forge.modules.tree.node.domain.NodeTreeIf;
+import org.springframework.lang.NonNull;
+
+import java.util.UUID;
+
+/**
+ * @author Lucas Batista Pereira
+ * @version v1.0
+ * @class NodeIfCreationData
+ * @since v1.0 (13/02/2025)
+ */
+
+public record NodeIfCreationData(@NonNull UUID parent_id, NodeTreeIf.SetNode position){}
---
+++ src/main/java/com/git/rule_forge/modules/tree/node/adapter/web/request/NodeTreeBlockData.java
@@ -0,0 +1,13 @@
+package com.git.rule_forge.modules.tree.node.adapter.web.request;
+
+/**
+ * @author Lucas Batista Pereira
+ * @version v1.0
+ * @class NodeTreeBlockData
+ * @since v1.0 (10/02/2025)
+ */
+
+public record NodeTreeBlockData(
+        String name
+) {
+}
