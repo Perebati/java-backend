6cc686eebc2621e42772ff5bdeb722a93b964004
perebati <lucas.bpereira1999@gmail.com>
2025-04-23 03:53:48 -0300

    [Feat]: Add comparison domain exceptions

---
+++ src/main/java/com/git/rule_forge/modules/tree/comparison/domain/exception/IllegalFieldException.java
@@ -0,0 +1,16 @@
+package com.git.rule_forge.modules.tree.comparison.domain.exception;
+
+import com.git.rule_forge.exception.SystemGlobalException;
+
+/**
+ * @author Lucas Batista Pereira
+ * @version v1.0
+ * @class IllegalArgumentException
+ * @since v1.0 (14/02/2025)
+ */
+
+public class IllegalFieldException extends SystemGlobalException {
+    public IllegalFieldException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
