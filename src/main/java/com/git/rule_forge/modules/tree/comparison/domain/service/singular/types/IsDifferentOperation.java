43f7ef08fb17aa10847286ddc6fb2db2449fb56a
perebati <lucas.bpereira1999@gmail.com>
2025-04-30 16:57:54 -0300

    [Feat]: Implement basic equality comparison operations

---
+++ src/main/java/com/git/rule_forge/modules/tree/comparison/domain/service/singular/types/IsDifferentOperation.java
@@ -0,0 +1,16 @@
+package com.git.rule_forge.modules.tree.comparison.domain.service.singular.types;
+
+import com.git.rule_forge.modules.tree.comparison.domain.service.singular.ComparisonSingularOperation;
+
+/**
+ * @author Lucas Batista Pereira
+ * @version v1.0
+ * @class IsDifferentOperation
+ * @since v1.0 (30/11/2024)
+ */
+public class IsDifferentOperation extends ComparisonSingularOperation {
+    @Override
+    public Boolean doOperation(String a, String b) {
+        return !a.equals(b);
+    }
+}
