ab23308d9f2215cd1aaf718823245eadcc6f45c6
perebati <lucas.bpereira1999@gmail.com>
2025-04-30 14:46:28 -0300

    [Feat]: Define comparison repository interfaces

---
+++ src/main/java/com/git/rule_forge/modules/tree/comparison/infrastructure/repository/interfaces/ComparisonMultiRepository.java
@@ -0,0 +1,13 @@
+package com.git.rule_forge.modules.tree.comparison.infrastructure.repository.interfaces;
+
+import com.git.rule_forge._shared.infrastructure.repository.interfaces.GenericBusinessRepository;
+import com.git.rule_forge.modules.tree.comparison.domain.ComparisonMulti;
+
+/**
+ * @author Lucas Batista Pereira
+ * @version v1.1
+ * @class ComparisonMultiRepository
+ * @since v1.0 (06/02/2025)
+ */
+public interface ComparisonMultiRepository extends GenericBusinessRepository<ComparisonMulti> {
+}
---
+++ src/main/java/com/git/rule_forge/modules/tree/comparison/infrastructure/repository/interfaces/ComparisonSingularRepository.java
@@ -0,0 +1,13 @@
+package com.git.rule_forge.modules.tree.comparison.infrastructure.repository.interfaces;
+
+import com.git.rule_forge._shared.infrastructure.repository.interfaces.GenericBusinessRepository;
+import com.git.rule_forge.modules.tree.comparison.domain.ComparisonSingular;
+
+/**
+ * @author Lucas Batista Pereira
+ * @version v1.0
+ * @class ComparisonOperationRepository
+ * @since v1.0 (30/11/2024)
+ */
+public interface ComparisonSingularRepository extends GenericBusinessRepository<ComparisonSingular> {
+}
